3aa7640989704e6fea7f277b36467c4f5ace54af
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-03-01 21:51:34 -0300

    Home started

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 function Home(){
     return(
-        <div id="home" className="container">
-            <h1>Hello World!</h1>
+        <div id="home" className="container-fluid">
+            <div id="homeHeader">
+                <h1>Thauan Rafael</h1>
+                <h2>Web Developer</h2>
+            </div>
+            <div id="sectionsButtons">
+                <button type="button" className="btn">About me</button>
+                <button type="button" className="btn">Skills</button>
+                <button type="button" className="btn">Projects</button>
+                <button type="button" className="btn">Contact</button>
+            </div>
         </div>
     )
 }
