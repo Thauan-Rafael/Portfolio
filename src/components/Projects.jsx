0e364291414b09e5e0905b2eb09d9790127ce505
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-03-05 20:42:14 -0300

    mobile versions started

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import printTest from '../media/printTest.png'
+import weatherImg from '../media/weatherIMG.png'
+import catsImg from '../media/catsIMG.png'
+import feedbackImg from '../media/feedbackIMG.png'
 
 function Projects(){
     return(
         <div id='projects'>
         <h2>Projects</h2>
             <div id='projectsList' className="row">
-                <ProjectCard title={cards[0].title} text={cards[0].text}/>
-                <ProjectCard title={cards[1].title} text={cards[1].text}/>
-                <ProjectCard title={cards[2].title} text={cards[2].text}/>
+                <ProjectCard title={cards[0].title} text={cards[0].text} image={cards[0].image}/>
+                <ProjectCard title={cards[1].title} text={cards[1].text} image={cards[1].image}/>
+                <ProjectCard title={cards[2].title} text={cards[2].text} image={cards[2].image}/>
             </div>
         <h3>More projects coming soon...</h3>    
         </div>    
@@ -17,7 +19,7 @@ function Projects(){
         return(
             <div className="card">
                 <div id='cardImage'>
-                <img className="card-img-top" src={printTest} alt="Card image cap"/>
+                <img className="card-img-top" src={props.image} alt="Card image cap"/>
                 </div>
                 <div className='card-header'>
                     <p>{props.title}</p>
@@ -35,23 +37,23 @@ function Projects(){
 }
 const cards = [
     {
-        image: '',
+        image: weatherImg,
         title: 'Weather Searcher',
         text: 'App to see a weather from a place searching by coordinates using OpenWeatherMap. Created with Node.js, Express.js, EJS and Bootstrap.',
         webLink: '',
         githubLink: ''  
     },
     {
-        image: '',
-        title: 'Give Me Feedback',
-        text: 'Web form that send feedbacks to a MongoDB database. Created with React,Bootstrap,Node.js, Express.js and MongoDB.',
+        image: catsImg,
+        title: 'Cats Chat',
+        text: 'Landing page of a software to adopt cats. Created with React and Bootstrap.',
         webLink: '',
         githubLink: ''
     },
     {
-        image: '',
-        title: 'Cats Chat',
-        text: 'Landing page of a software to adopt cats. Created with React and Bootstrap.',
+        image: feedbackImg,
+        title: 'Give Me Feedback',
+        text: 'Web form that send feedbacks to a MongoDB database. Created with React,Bootstrap,Node.js, Express.js and MongoDB.',
         webLink: '',
         githubLink: ''
     }
