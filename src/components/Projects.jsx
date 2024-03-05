23b50b5ee7cc025e66329778d10627b89f1ebac7
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-03-04 21:42:00 -0300

    Skills started 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,9 @@ import reactImg from '../media/react.png'
 
 function Projects(){
     return(
-        <div id='projects' className='row'>
+        <div id='projects'>
         <h2>Projects</h2>
-            <div className="projectsList col-sm-6 col-lg-4">
+            <div id='projectsList' className="row">
                 <ProjectCard/>
                 <ProjectCard/>
                 <ProjectCard/>
@@ -17,11 +17,17 @@ function Projects(){
         return(
             <div className="card">
                 <img className="card-img-top" src={reactImg} alt="Card image cap"/>
+                <div className='card-header'>Header</div>
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div id='cardButtons'>
+                        <button className='btn btn-success'>Hello</button>
+                        <button className='btn btn-success'>World</button>
+                    </div>
                 </div>
             </div>
         )
     }
 }
+//col-sm-6 col-lg-4
 export default Projects;
