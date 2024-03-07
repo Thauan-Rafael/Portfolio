91425faf67f0f898a03b23d1321497109174d74e
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-03-07 17:48:32 -0300

    Started the redirect of the buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,20 @@ function Home(){
                 <h1>Thauan Rafael</h1>
                 <h2>Web Developer</h2>
                 <div id="homeButtons">
-                    <button type="button" className="icon fa-brands fa-github"></button>
-                    <button type="button" className="icon fa-brands fa-linkedin"></button>
+                    <button id="githubBtn" type="button" className="icon fa-brands fa-github" onClick={socialLink}></button>
+                    <button id="LinkedinBtn" type="button" className="icon fa-brands fa-linkedin" onClick={socialLink}></button>
                 </div>
             </div>
         </div>
     )
+}
+function socialLink(event){
+    const buttonClicked = event.target.id;
+    if(buttonClicked == 'githubBtn'){
+        window.open('https://github.com/Thauan-Rafael','_blank').focus();
+    }
+    else{
+        window.open('https://br.linkedin.com/in/thauan-rafael-0556a7294','_blank').focus();
+    }
 }
 export default Home;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,11 +3,35 @@ import React from 'react'
 function SideBar(){
     return(
         <div id='sideBar'>
-            <i className="icon fa-solid fa-home fa-2x"></i>
-            <i className="icon fa-solid fa-user fa-2x"></i>
-            <i className="icon fa-solid fa-lightbulb fa-2x"></i>
-            <i className="icon fa-solid fa-folder fa-2x"></i>
+            <i id='barLink1' className="icon fa-solid fa-home fa-2x" onClick={redirectUser}></i>
+            <i id='barLink2' className="icon fa-solid fa-user fa-2x" onClick={redirectUser}></i>
+            <i id='barLink3' className="icon fa-solid fa-lightbulb fa-2x" onClick={redirectUser}></i>
+            <i id='barLink4' className="icon fa-solid fa-folder fa-2x" onClick={redirectUser}></i>
         </div>
     )
+}
+function redirectUser(event){
+    const clickedLink = event.target.id;
+    let redirect = '';
+    switch (clickedLink) {
+        case 'barLink1':
+            redirect = document.getElementById('home');
+            redirect.scrollIntoView({behavior: 'smooth'}) 
+            break;
+        case 'barLink2':
+            redirect = document.getElementById('about');
+            redirect.scrollIntoView({behavior: 'smooth'}) 
+            break;
+        case 'barLink3':
+            redirect = document.getElementById('skills');
+            redirect.scrollIntoView({behavior: 'smooth'}) 
+            break;
+    case 'barLink4':
+            redirect = document.getElementById('projects');
+            redirect.scrollIntoView({behavior: 'smooth'}) 
+            break;        
+        default:
+            break;
+    }
 }
 export default SideBar;
